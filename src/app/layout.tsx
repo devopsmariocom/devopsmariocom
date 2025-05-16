0ae23904deeb27041e391bf6b31691925e76d7e0
DevOpsMario <dev@devopsmario.com>
2025-05-16 14:45:23 +0200

    Use nextjs/deploy-github-pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,6 @@ export default function RootLayout({
   return (
     <html lang="cs">
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
       </head>
       <body className="relative min-h-screen overflow-x-hidden">
         {children}
