53c31d0d2ba41ed1e58192c3a6dbd3e96ea5599e
DevOpsMario <dev@devopsmario.com>
2025-05-16 11:45:40 +0200

    Configure base path and asset prefix for production environment in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/devopsmariocom' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/devopsmariocom/' : '',
 }
 
 module.exports = nextConfig 
